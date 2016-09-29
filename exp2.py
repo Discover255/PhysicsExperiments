7baac9e05620d3bb5b416b04d4b68fc103447e0e
lim <1215223072@qq.com>
2016-09-29 20:08:06 +0800

    experiment2

---
+++ exp2.py
@@ -0,0 +1,8 @@
+import math
+import uncertainty
+L = input("输入D(以下以毫米为单位)\n")
+D = input("输入D\n")
+H = input("输入H\n")
+d = input("输入直径\n")
+d0 = input("输入零差\n")
+
--- uncertainty.py
+++ uncertainty.py
@@ -1,11 +1,8 @@
 #coding:utf-8
 import math
-def getLength(reData):
-	reDatas = reData.split()
-	return len(reDatas)
 def stringToNums(reData):
 	reDatas = reData.split()
-	length = getLength(reData)
+	length = len(reDatas)
 	nums = [0]*length
 	for i in range(length):
 		nums[i] = float(reDatas[i])
@@ -17,21 +14,23 @@ def calVari(nums):
 		singOfValues = (nums[i]-averOfNums)**2
 		sumOfValues += singOfValues
 	return sumOfValues/len(nums) 
-def getBessel(expResults):
-	numsResults = stringToNums(expResults)
+def getBessel(numsResults):
 	variOfResults = calVari(numsResults)
 	return math.sqrt(variOfResults)
 def getMultiple(nums):
-	multiples = {3:4.3 , 4:3.18 , 5:2.78 , 6:2.57 , 7:2.45 , 8:2.36 , 9:2.31 , 10:2.26 , 15:2.14 , 20:2.09}
-	return multiples.get(nums)/math.sqrt(nums)
-datas = raw_input('输入实验结果\n')
-aClass = getBessel(datas)*getMultiple(getLength(datas))
-print('A类不确定度为%f'%aClass)
+	if nums<3:
+		print("输入的数据小于三组，无法计算")
+		return 0.0
+	else:
+		multiples = {3:4.3 , 4:3.18 , 5:2.78 , 6:2.57 , 7:2.45 , 8:2.36 , 9:2.31 , 10:2.26 , 15:2.14 , 20:2.09}
+		return multiples.get(nums)/math.sqrt(nums)
+#aClass = getBessel(stringToNums(datas))*getMultiple(len(stringToNums(datas)))
+#这是A类不确定度计算总式print('A类不确定度为%f'%aClass)
+#precsn = input('输入最小刻度值\n')
 def getBClass(precsn):
-	precsn = input('输入最小刻度值\n')
 	return precsn/2
 def getUncertn(a,b):
 	return math.sqrt((a**2)+(b**2))
 #print('不确定度为%f'%getUncertn(aClass,getBClass)
-a = getUncertn(aClass,getBClass(6))
-print(a)
+#a = getUncertn(aClass,getBClass(float(precsn)))
+#print(a)
